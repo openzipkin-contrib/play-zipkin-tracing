7a8e8516982b827eefe4120775ac8b661d1a41f0
Naoki Takezoe <takezoe@gmail.com>
2017-02-17 12:50:06 +0900

    Fixup

--- play-zipkin-tracing/core/src/main/scala/jp/co/bizreach/trace/TraceData.scala
+++ play-zipkin-tracing/core/src/main/scala/jp/co/bizreach/trace/TraceData.scala
@@ -1,9 +1,7 @@
 package jp.co.bizreach.trace.zipkin
 
-import jp.co.bizreach.trace.TraceCassette
-
 /**
   * Created by nishiyama on 2016/12/08.
   */
-case class ZipkinTraceCassette(span: brave.Span) extends TraceCassette
+case class TraceData(span: brave.Span)
 
