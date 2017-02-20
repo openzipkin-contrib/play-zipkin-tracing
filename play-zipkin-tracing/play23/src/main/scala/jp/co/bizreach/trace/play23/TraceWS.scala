eb9d1c5442ca6baac2003a8ea15c11e50625e79f
Naoki Takezoe <takezoe@gmail.com>
2017-02-20 13:15:49 +0900

    Add scaladoc

--- play-zipkin-tracing/play23/src/main/scala/jp/co/bizreach/trace/play23/TraceWS.scala
+++ play-zipkin-tracing/play23/src/main/scala/jp/co/bizreach/trace/play23/TraceWS.scala
@@ -7,49 +7,45 @@ import play.api.libs.ws._
 
 import scala.concurrent.Future
 
+/**
+ * Use this instead of WS to trace service calling.
+ */
 object TraceWS {
 
+  /**
+   * Generates a request holder with the span name which is used for tracing.
+   * B3 headers are added to the generated request holder and tracing data is sent to the zipkin server automatically.
+   */
   def url(spanName: String, url: String)(implicit app: Application, traceData: TraceData): play.api.libs.ws.WSRequestHolder = {
     new TraceWSRequest(spanName, WS.url(url).withHeaders(ZipkinTraceService.toMap(traceData).toSeq: _*), traceData)
   }
 
-}
+  private class TraceWSRequest(spanName: String, request: WSRequestHolder, traceData: TraceData) extends WSRequestHolder {
+    override val url: String = request.url
+    override val method: String = request.method
+    override val body: WSBody = request.body
+    override val headers: Map[String, Seq[String]] = request.headers
+    override val queryString: Map[String, Seq[String]] = request.queryString
+    override val calc: Option[WSSignatureCalculator] = request.calc
+    override val auth: Option[(String, String, WSAuthScheme)] = request.auth
+    override val followRedirects: Option[Boolean] = request.followRedirects
+    override val requestTimeout: Option[Int] = request.requestTimeout
+    override val virtualHost: Option[String] = request.virtualHost
+    override val proxyServer: Option[WSProxyServer] = request.proxyServer
 
-class TraceWSRequest(spanName: String, request: WSRequestHolder, traceData: TraceData) extends WSRequestHolder {
-  override val url: String = request.url
-  override val method: String = request.method
-  override val body: WSBody = request.body
-  override val headers: Map[String, Seq[String]] = request.headers
-  override val queryString: Map[String, Seq[String]] = request.queryString
-  override val calc: Option[WSSignatureCalculator] = request.calc
-  override val auth: Option[(String, String, WSAuthScheme)] = request.auth
-  override val followRedirects: Option[Boolean] = request.followRedirects
-  override val requestTimeout: Option[Int] = request.requestTimeout
-  override val virtualHost: Option[String] = request.virtualHost
-  override val proxyServer: Option[WSProxyServer] = request.proxyServer
-
-  override def sign(calc: SignatureCalculator): WSRequestHolder = new TraceWSRequest(spanName, request.sign(calc), traceData)
-
-  override def withAuth(username: String, password: String, scheme: WSAuthScheme): WSRequestHolder = new TraceWSRequest(spanName, request.withAuth(username, password, scheme), traceData)
-
-  override def withHeaders(hdrs: (String, String)*): WSRequestHolder = new TraceWSRequest(spanName, request.withHeaders(hdrs:_*), traceData)
-
-  override def withQueryString(parameters: (String, String)*): WSRequestHolder = new TraceWSRequest(spanName, request.withQueryString(parameters:_*), traceData)
-
-  override def withFollowRedirects(follow: Boolean): WSRequestHolder = new TraceWSRequest(spanName, request.withFollowRedirects(follow), traceData)
-
-  override def withRequestTimeout(timeout: Int): WSRequestHolder = new TraceWSRequest(spanName, request.withRequestTimeout(timeout), traceData)
-
-  override def withVirtualHost(vh: String): WSRequestHolder = new TraceWSRequest(spanName, request.withVirtualHost(vh), traceData)
-
-  override def withProxyServer(proxyServer: WSProxyServer): WSRequestHolder = new TraceWSRequest(spanName, request.withProxyServer(proxyServer), traceData)
-
-  override def withBody(body: WSBody): WSRequestHolder = new TraceWSRequest(spanName, request.withBody(body), traceData)
-
-  override def withMethod(method: String): WSRequestHolder = new TraceWSRequest(spanName, request.withMethod(method), traceData)
-
-  override def execute(): Future[Response] = ZipkinTraceService.traceFuture(spanName){ request.execute() }(traceData)
-
-  override def stream(): Future[(WSResponseHeaders, Enumerator[Array[Byte]])] = ZipkinTraceService.traceFuture(spanName){ request.stream() }(traceData)
+    override def sign(calc: SignatureCalculator): WSRequestHolder = new TraceWSRequest(spanName, request.sign(calc), traceData)
+    override def withAuth(username: String, password: String, scheme: WSAuthScheme): WSRequestHolder = new TraceWSRequest(spanName, request.withAuth(username, password, scheme), traceData)
+    override def withHeaders(hdrs: (String, String)*): WSRequestHolder = new TraceWSRequest(spanName, request.withHeaders(hdrs:_*), traceData)
+    override def withQueryString(parameters: (String, String)*): WSRequestHolder = new TraceWSRequest(spanName, request.withQueryString(parameters:_*), traceData)
+    override def withFollowRedirects(follow: Boolean): WSRequestHolder = new TraceWSRequest(spanName, request.withFollowRedirects(follow), traceData)
+    override def withRequestTimeout(timeout: Int): WSRequestHolder = new TraceWSRequest(spanName, request.withRequestTimeout(timeout), traceData)
+    override def withVirtualHost(vh: String): WSRequestHolder = new TraceWSRequest(spanName, request.withVirtualHost(vh), traceData)
+    override def withProxyServer(proxyServer: WSProxyServer): WSRequestHolder = new TraceWSRequest(spanName, request.withProxyServer(proxyServer), traceData)
+    override def withBody(body: WSBody): WSRequestHolder = new TraceWSRequest(spanName, request.withBody(body), traceData)
+    override def withMethod(method: String): WSRequestHolder = new TraceWSRequest(spanName, request.withMethod(method), traceData)
+    override def execute(): Future[Response] = ZipkinTraceService.traceFuture(spanName){ request.execute() }(traceData)
+    override def stream(): Future[(WSResponseHeaders, Enumerator[Array[Byte]])] = ZipkinTraceService.traceFuture(spanName){ request.stream() }(traceData)
+  }
 
 }
+
--- play-zipkin-tracing/play25/src/main/scala/jp/co/bizreach/trace/play25/TraceWSClient.scala
+++ play-zipkin-tracing/play25/src/main/scala/jp/co/bizreach/trace/play25/TraceWSClient.scala
@@ -11,14 +11,21 @@ import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 
 /**
- * Created by naoki.takezoe on 2017/02/17.
+ * Inject this class instead of WSClient to trace service calling.
  */
 class TraceWSClient @Inject()(ws: WSClient, tracer: ZipkinTraceServiceLike) {
 
   def underlying[T]: T = ws.underlying
 
+  /**
+   * Generates a request holder without tracing.
+   */
   def url(url: String): WSRequest = ws.url(url)
 
+  /**
+   * Generates a request holder with the span name which is used for tracing.
+   * B3 headers are added to the generated request holder and tracing data is sent to the zipkin server automatically.
+   */
   def url(spanName: String, url: String)(implicit traceData: TraceData): WSRequest = {
     new TraceWSRequest(spanName, ws.url(url), tracer, traceData).withHeaders(tracer.toMap(traceData).toSeq: _*)
   }
@@ -26,49 +33,37 @@ class TraceWSClient @Inject()(ws: WSClient, tracer: ZipkinTraceServiceLike) {
   @scala.throws[IOException]
   def close(): Unit = ws.close()
 
+  private class TraceWSRequest(spanName: String, request: WSRequest, tracer: ZipkinTraceServiceLike, traceData: TraceData) extends WSRequest {
+
+    override val url: String = request.url
+    override val method: String = request.method
+    override val body: WSBody = request.body
+    override val headers: Map[String, Seq[String]] = request.headers
+    override val queryString: Map[String, Seq[String]] = request.queryString
+    override val calc: Option[WSSignatureCalculator] = request.calc
+    override val auth: Option[(String, String, WSAuthScheme)] = request.auth
+    override val followRedirects: Option[Boolean] = request.followRedirects
+    override val requestTimeout: Option[Int] = request.requestTimeout
+    override val virtualHost: Option[String] = request.virtualHost
+    override val proxyServer: Option[WSProxyServer] = request.proxyServer
+
+    override def sign(calc: WSSignatureCalculator): WSRequest = new TraceWSRequest(spanName, request.sign(calc), tracer, traceData)
+    override def withAuth(username: String, password: String, scheme: WSAuthScheme): WSRequest = new TraceWSRequest(spanName, request.withAuth(username, password, scheme), tracer, traceData)
+    override def withHeaders(hdrs: (String, String)*): WSRequest = new TraceWSRequest(spanName, request.withHeaders(hdrs:_*), tracer, traceData)
+    override def withQueryString(parameters: (String, String)*): WSRequest = new TraceWSRequest(spanName, request.withQueryString(parameters:_*), tracer, traceData)
+    override def withFollowRedirects(follow: Boolean): WSRequest = new TraceWSRequest(spanName, request.withFollowRedirects(follow), tracer, traceData)
+    override def withRequestTimeout(timeout: Duration): WSRequest = new TraceWSRequest(spanName, request.withRequestTimeout(timeout), tracer, traceData)
+    override def withRequestFilter(filter: WSRequestFilter): WSRequest = new TraceWSRequest(spanName, request.withRequestFilter(filter), tracer, traceData)
+    override def withVirtualHost(vh: String): WSRequest = new TraceWSRequest(spanName, request.withVirtualHost(vh), tracer, traceData)
+    override def withProxyServer(proxyServer: WSProxyServer): WSRequest = new TraceWSRequest(spanName, request.withProxyServer(proxyServer), tracer, traceData)
+    override def withBody(body: WSBody): WSRequest = new TraceWSRequest(spanName, request.withBody(body), tracer, traceData)
+    override def withMethod(method: String): WSRequest = new TraceWSRequest(spanName, request.withMethod(method), tracer, traceData)
+    override def execute(): Future[WSResponse] = tracer.traceFuture(spanName){ request.execute() }(traceData)
+    override def stream(): Future[StreamedResponse] = tracer.traceFuture(spanName){ request.stream() }(traceData)
+
+    @scala.deprecated("Use `WS.stream()` instead.")
+    override def streamWithEnumerator(): Future[(WSResponseHeaders, Enumerator[Array[Byte]])] = tracer.traceFuture(spanName){ request.streamWithEnumerator() }(traceData)
+
+  }
 }
 
-class TraceWSRequest(spanName: String, request: WSRequest, tracer: ZipkinTraceServiceLike, traceData: TraceData) extends WSRequest {
-
-  override val url: String = request.url
-  override val method: String = request.method
-  override val body: WSBody = request.body
-  override val headers: Map[String, Seq[String]] = request.headers
-  override val queryString: Map[String, Seq[String]] = request.queryString
-  override val calc: Option[WSSignatureCalculator] = request.calc
-  override val auth: Option[(String, String, WSAuthScheme)] = request.auth
-  override val followRedirects: Option[Boolean] = request.followRedirects
-  override val requestTimeout: Option[Int] = request.requestTimeout
-  override val virtualHost: Option[String] = request.virtualHost
-  override val proxyServer: Option[WSProxyServer] = request.proxyServer
-
-  override def sign(calc: WSSignatureCalculator): WSRequest = new TraceWSRequest(spanName, request.sign(calc), tracer, traceData)
-
-  override def withAuth(username: String, password: String, scheme: WSAuthScheme): WSRequest = new TraceWSRequest(spanName, request.withAuth(username, password, scheme), tracer, traceData)
-
-  override def withHeaders(hdrs: (String, String)*): WSRequest = new TraceWSRequest(spanName, request.withHeaders(hdrs:_*), tracer, traceData)
-
-  override def withQueryString(parameters: (String, String)*): WSRequest = new TraceWSRequest(spanName, request.withQueryString(parameters:_*), tracer, traceData)
-
-  override def withFollowRedirects(follow: Boolean): WSRequest = new TraceWSRequest(spanName, request.withFollowRedirects(follow), tracer, traceData)
-
-  override def withRequestTimeout(timeout: Duration): WSRequest = new TraceWSRequest(spanName, request.withRequestTimeout(timeout), tracer, traceData)
-
-  override def withRequestFilter(filter: WSRequestFilter): WSRequest = new TraceWSRequest(spanName, request.withRequestFilter(filter), tracer, traceData)
-
-  override def withVirtualHost(vh: String): WSRequest = new TraceWSRequest(spanName, request.withVirtualHost(vh), tracer, traceData)
-
-  override def withProxyServer(proxyServer: WSProxyServer): WSRequest = new TraceWSRequest(spanName, request.withProxyServer(proxyServer), tracer, traceData)
-
-  override def withBody(body: WSBody): WSRequest = new TraceWSRequest(spanName, request.withBody(body), tracer, traceData)
-
-  override def withMethod(method: String): WSRequest = new TraceWSRequest(spanName, request.withMethod(method), tracer, traceData)
-
-  override def execute(): Future[WSResponse] = tracer.traceFuture(spanName){ request.execute() }(traceData)
-
-  override def stream(): Future[StreamedResponse] = tracer.traceFuture(spanName){ request.stream() }(traceData)
-
-  @scala.deprecated("Use `WS.stream()` instead.")
-  override def streamWithEnumerator(): Future[(WSResponseHeaders, Enumerator[Array[Byte]])] = tracer.traceFuture(spanName){ request.streamWithEnumerator() }(traceData)
-
-}
