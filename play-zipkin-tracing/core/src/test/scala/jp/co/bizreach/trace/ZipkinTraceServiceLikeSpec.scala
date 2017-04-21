b838a57e6413ca9215bb9607c42cee20c9e81eed
Naoki Takezoe <takezoe@gmail.com>
2017-04-21 11:53:14 +0900

    Add test for ZipkinTraceServiceLike

--- play-zipkin-tracing/core/src/test/scala/jp/co/bizreach/trace/ZipkinTraceServiceLikeSpec.scala
+++ play-zipkin-tracing/core/src/test/scala/jp/co/bizreach/trace/ZipkinTraceServiceLikeSpec.scala
@@ -6,7 +6,8 @@ import zipkin.Span
 import zipkin.reporter.Reporter
 
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 
 class ZipkinTraceServiceLikeSpec extends FunSuite {
@@ -15,7 +16,7 @@ class ZipkinTraceServiceLikeSpec extends FunSuite {
     TraceData(tracer.newSpan[Map[String, String]](header)((headers: Map[String, String], key: String) => headers.get(key)))
   }
 
-  test("Nested client tracing"){
+  test("Nested local synchronous tracing"){
     val tracer = new TestZipkinTraceService()
     implicit val traceData = initialTraceData(tracer, Map.empty)
 
@@ -25,7 +26,6 @@ class ZipkinTraceServiceLikeSpec extends FunSuite {
       }
     }
 
-    Thread.sleep(500)
     assert(tracer.reporter.spans.length == 2)
 
     val parent = tracer.reporter.spans.find(_.name == "trace-1").get
@@ -35,6 +35,34 @@ class ZipkinTraceServiceLikeSpec extends FunSuite {
     assert(parent.id != child.id)
     assert(parent.duration > child.duration)
   }
+
+  test("Future and a nested local synchronous process tracing") {
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    val tracer = new TestZipkinTraceService()
+    implicit val traceData = initialTraceData(tracer, Map.empty)
+
+    val f = tracer.traceFuture("trace-future") { implicit traceData =>
+      Future {
+        tracer.trace("trace-sync") { _ =>
+          Thread.sleep(500)
+        }
+      }
+    }
+
+    Await.result(f, Duration.Inf)
+    Thread.sleep(100) // wait for callback completion
+
+    assert(tracer.reporter.spans.length == 2)
+    val parent = tracer.reporter.spans.find(_.name == "trace-future").get
+    val child  = tracer.reporter.spans.find(_.name == "trace-sync").get
+
+    assert(parent.duration >= 500)
+    assert(parent.id == child.parentId)
+    assert(parent.id != child.id)
+    assert(parent.duration > child.duration)
+  }
+
 
 }
 
